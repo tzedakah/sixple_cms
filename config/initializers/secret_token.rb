afbe1ff57a5ee7fa2ba10f7332dc181580f0c06b
Antonio Chinea <creative.studio.acts@gmail.com>
2011-12-09 09:56:06 -0430

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SixpleCms::Application.config.secret_token = '80a26b101aa5253e753682d42d759e1a7789b633eb9aaed68d995a9b2901133c6a1cd4ece9672c9cc5a2b4279bf7a66cc5d918637809a94b73995a6a203604c9'
